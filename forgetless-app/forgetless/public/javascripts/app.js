d89c6af2de150d9db7b2ad71fb9a5ca2b4f0783a
Greg Joy <gregjoy91@gmail.com>
2014-01-15 16:09:51 +0000

    added more to frontend... (sync)

--- forgetless-app/forgetless/public/javascripts/app.js
+++ forgetless-app/forgetless/public/javascripts/app.js
@@ -1,14 +1,24 @@
-forgetlessApp.config(function($routeProvider) {
-    $routeProvider.
-    when('/', {
+forgetlessApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('app', {
+        url: '/',
         controller: 'ContentController',
         templateUrl: '/partials/listView.html'
-    }).
-    when('/login', {
+    });
+
+    $stateProvider.state('app.category', {
+        url: '/:category',
+        controller: 'ContentController',
+        templateUrl: '/partials/listView.html'
+    });
+
+    $stateProvider.state('login', {
+        url: '/login',
+//        controller: 'LoginController',
         controller: 'LoginController',
         templateUrl: '/partials/loginView.html'
-    }).
-    otherwise({
-        redirectTo: '/'
     });
+
 });
--- forgetless-app/forgetless/public/javascripts/forgetless.js
+++ forgetless-app/forgetless/public/javascripts/forgetless.js
@@ -1,10 +1,49 @@
-var forgetlessApp = angular.module('forgetlessApp', ['ngRoute']);
+var forgetlessApp = angular.module('forgetlessApp', ['ui.router']);
+
+forgetlessApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('app', {
+        url: '/',
+        controller: 'ContentViewPointController',
+        templateUrl: '/partials/listView.html'
+    });
+
+    $stateProvider.state('app.category', {
+        url: ':categoryId',
+        controller: 'ContentViewPointController',
+        templateUrl: '/partials/listView.html'
+    });
+
+    $stateProvider.state('app.category.list', {
+        url: '/:listId',
+        controller: 'ContentViewPointController',
+        templateUrl: '/partials/listView.html'
+    });
+
+    $stateProvider.state('app.category.list.item', {
+        url: '/:itemId',
+        controller: 'ContentViewPointController',
+        templateUrl: '/partials/listView.html'
+    });
+
+    $stateProvider.state('login', {
+        url: '/login',
+        controller: 'LoginController',
+        templateUrl: '/partials/loginView.html'
+    });
+
+});
 
 forgetlessApp.controller('ContentViewPointController', function($scope) {
     $scope.stepClass = '';
 
     $scope.nextStep = function(step) {
         switch(step) {
+            case 0:
+                $scope.stepClass = 'step-zero';
+                break;
             case 1:
                 $scope.stepClass = 'step-one';
                 break;
@@ -18,18 +57,33 @@ forgetlessApp.controller('ContentViewPointController', function($scope) {
                 $scope.stepClass = '';
                 break;
         }
-    }
+    };
 
 });
 
-forgetlessApp.controller('ContentController', function($scope) {});
+forgetlessApp.controller('ContentController', function($scope, $stateParams, $state) {
+    if($stateParams.itemId !== undefined && $stateParams.itemId != '') {
+        $scope.nextStep(3);
+    } else if($stateParams.listId !== undefined && $stateParams.listId != '') {
+        $scope.nextStep(2);
+    } else if($stateParams.categoryId !== undefined && $stateParams.categoryId != '') {
+        $scope.nextStep(1);
+    } else {
+        $scope.nextStep(0);
+    }
+    // keep beady eye on this...
+    $state.reload();
+});
 
-forgetlessApp.controller('CategoryController', function($scope) {
+forgetlessApp.controller('CategoryController', function($scope, $stateParams) {
 
     $scope.categories = [];
 
     for(var inc = 1; inc < 20; inc++) {
-        $scope.categories.push('Category ' + inc);
+        $scope.categories.push({
+            id: inc,
+            title: 'Category ' + inc
+        });
     }
 
 });
@@ -39,7 +93,10 @@ forgetlessApp.controller('ListController', function($scope) {
     $scope.lists = [];
 
     for(var inc = 1; inc < 20; inc++) {
-        $scope.lists.push('List ' + inc);
+        $scope.lists.push({
+            id: inc,
+            title: 'List ' + inc
+        });
     }
 
 });
@@ -49,7 +106,10 @@ forgetlessApp.controller('ItemController', function($scope) {
     $scope.items = [];
 
     for(var inc = 1; inc < 20; inc++) {
-        $scope.items.push('Item ' + inc);
+        $scope.items.push({
+            id: inc,
+            title: 'Item ' + inc
+        });
     }
 
 });
@@ -79,5 +139,11 @@ forgetlessApp.directive('syncHeight', function($window) {
 });
 
 forgetlessApp.controller('LoginController', function($scope) {
+    $scope.testing = 123;
+    $scope.testfunc = function() {
+        console.log('tetsing');
+    };
+
+    $scope.testfunc();
 
 });
