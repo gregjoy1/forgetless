6012ad6ca147a6c50b7d96d41423ed55b633fa57
Greg Joy <gregjoy91@gmail.com>
2014-01-18 17:08:14 +0000

    sync + frontend

--- forgetless-app/forgetless/public/javascripts/app.js
+++ forgetless-app/forgetless/public/javascripts/app.js
@@ -5,20 +5,20 @@ forgetlessApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('app', {
         url: '/',
         controller: 'ContentController',
-        templateUrl: '/partials/listView.html'
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('app.category', {
         url: '/:category',
         controller: 'ContentController',
-        templateUrl: '/partials/listView.html'
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('login', {
         url: '/login',
 //        controller: 'LoginController',
         controller: 'LoginController',
-        templateUrl: '/partials/loginView.html'
+        templateUrl: '/partials/login_view.html'
     });
 
 });
--- forgetless-app/forgetless/public/javascripts/forgetless.js
+++ forgetless-app/forgetless/public/javascripts/forgetless.js
@@ -7,31 +7,40 @@ forgetlessApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('app', {
         url: '/',
         controller: 'ContentViewPointController',
-        templateUrl: '/partials/listView.html'
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('app.category', {
         url: ':categoryId',
-        controller: 'ContentViewPointController',
-        templateUrl: '/partials/listView.html'
+//        controller: 'ContentViewPointController',
+        controller: function($scope, $stateParams) {
+            console.log($stateParams);
+        },
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('app.category.list', {
         url: '/:listId',
-        controller: 'ContentViewPointController',
-        templateUrl: '/partials/listView.html'
+//        controller: 'ContentViewPointController',
+        controller: function($scope, $stateParams) {
+            console.log($stateParams);
+        },
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('app.category.list.item', {
         url: '/:itemId',
-        controller: 'ContentViewPointController',
-        templateUrl: '/partials/listView.html'
+        controller: function($scope, $stateParams) {
+            console.log($stateParams);
+        },
+//        controller: 'ContentViewPointController',
+        templateUrl: '/partials/list_view.html'
     });
 
     $stateProvider.state('login', {
         url: '/login',
         controller: 'LoginController',
-        templateUrl: '/partials/loginView.html'
+        templateUrl: '/partials/login_view.html'
     });
 
 });
@@ -56,30 +65,28 @@ forgetlessApp.controller('ContentViewPointController', function($scope) {
 
 forgetlessApp.controller('ContentController', function($scope, $stateParams, $state, $window) {
 
-    var routeSteps = function() {
+    $scope.routeSteps = function() {
         if($stateParams.categoryId !== undefined && $stateParams.categoryId != '') {
             $scope.nextStep(1);
         } else {
             $scope.nextStep(0);
         }
+        console.log($stateParams);
     };
+
 
 //    angular.element($window).bind('popstate', routeSteps);
 
-    // tempin!
-    angular.element($window).bind('popstate', function() {
-        $scope.nextStep(0);
-    });
-
-    routeSteps();
+    $scope.routeSteps();
 
     $scope.selectedCategoryIndex = 0;
+    $scope.selectedCategoryId = -1;
     $scope.selectedListIndex = 0;
     $scope.selectedItemIndex = 0;
 
     $scope.categories = [];
 
-    for(var catInc = 0; catInc <  20; catInc++) {
+    for(var catInc = 0; catInc <  5; catInc++) {
 
         $scope.categories.push({
             id: catInc,
@@ -88,22 +95,22 @@ forgetlessApp.controller('ContentController', function($scope, $stateParams, $st
             lists: []
         });
 
-        for(var listInc = 0; listInc <  20; listInc++) {
+        for(var listInc = 0; listInc <  10; listInc++) {
 
             $scope.categories[catInc].lists.push({
                 id: listInc,
-                title: 'title: ' + catInc + ' : ' + listInc,
-                selected: (listInc == 0),
+                title: 'list: ' + catInc + ' : ' + listInc,
+                selected: false,
                 items: []
             });
 
-            for(var itemInc = 0; itemInc <  20; itemInc++) {
+            for(var itemInc = 0; itemInc <  10; itemInc++) {
 
                 $scope.categories[catInc].lists[listInc].items.push({
                     id: itemInc,
                     title: 'item: ' + catInc + ' : ' + listInc + ' : ' + itemInc,
                     content: 'im a content-der ' + itemInc,
-                    selected: (itemInc == 0)
+                    selected: false
                 });
 
             }
@@ -122,20 +129,19 @@ forgetlessApp.controller('ContentController', function($scope, $stateParams, $st
             if($scope.categories[inc].id == id) {
                 $scope.categories[inc].selected = true;
                 $scope.selectedCategoryIndex = inc;
+                $scope.selectedCategoryId = $scope.categories[inc].id;
+                $state.go('app.category', {categoryId: id});
             } else {
                 $scope.categories[inc].selected = false;
             }
         }
 
-        $scope.nextStep(1);
+//        $scope.nextStep(1);
+//        $scope.routeSteps();
 
     };
 
     $scope.selectList = function(id) {
-
-        // Falls back on first index if fail
-        $scope.selectedListIndex = 0;
-
         // Save on width...
         var catIndex = $scope.selectedCategoryIndex;
 
@@ -143,19 +149,21 @@ forgetlessApp.controller('ContentController', function($scope, $stateParams, $st
         var numberOfLists = $scope.categories[catIndex].lists.length;
         for(var inc = 0; inc < numberOfLists; inc++) {
             if($scope.categories[catIndex].lists[inc].id == id) {
-                $scope.categories[catIndex].lists[inc].selected = true;
-                $scope.selectedListIndex = inc;
-            } else {
-                $scope.categories[catIndex].lists[inc].selected = false;
+                var selected = $scope.categories[catIndex].lists[inc].selected;
+                $scope.categories[catIndex].lists[inc].selected = !selected;
+
+                $state.go('app.category.list', {
+                    categoryId: $scope.selectedCategoryId,
+                    listId: id
+                });
+
+                break;
             }
         }
+//        $scope.routeSteps();
     };
 
     $scope.selectItem = function(id) {
-
-        // Falls back on first index if fail
-        $scope.selectedItemIndex = 0;
-
         // Save on width...
         var catIndex = $scope.selectedCategoryIndex;
         var lstIndex = $scope.selectedListIndex;
@@ -164,24 +172,65 @@ forgetlessApp.controller('ContentController', function($scope, $stateParams, $st
         var numberOfItems = $scope.categories[catIndex].lists[lstIndex].items.length;
         for(var inc = 0; inc < numberOfItems; inc++) {
             if($scope.categories[catIndex].lists[lstIndex].items[inc].id == id) {
-                $scope.categories[catIndex].lists[lstIndex].items[inc].selected = true;
-                $scope.selectedListIndex = inc;
-            } else {
-                $scope.categories[catIndex].lists[lstIndex].items[inc].selected = false;
+                var selected = $scope.categories[catIndex].lists[lstIndex].items[inc].selected;
+                $scope.categories[catIndex].lists[lstIndex].items[inc].selected = !selected;
+                break;
             }
+        }
+//        $scope.routeSteps();
+    };
+});
+
+forgetlessApp.directive('handleCategory', function() {
+
+    return {
+        templateUrl: '/partials/category_item_view.html',
+        controller: function($scope, $element) {
+            var element = $element[0];
+            var categoryHeader = element.querySelector('.content-item');
+
+//            angular.element(categoryHeader).on('click', function() {});
+
+        }
+    };
+
+
+});
+
+forgetlessApp.directive('handleList', function() {
+
+    return {
+        templateUrl: '/partials/list_item_view.html',
+        controller: function($scope, $element) {
+            var element = $element[0];
+            var listHeader = element.querySelector('.content-item');
+            var itemsContainer = element.querySelector('.items-container');
         }
     };
 
 });
 
+forgetlessApp.directive('handleItem', function($state) {
 
-forgetlessApp.directive('testingStuff', function() {
+    return {
+        templateUrl: '/partials/item_view.html',
+        controller: function($scope, $element) {
+            var element = $element[0];
+            var itemHeader = element.querySelector('.content-item');
+            var itemDetails = element.querySelector('.item-detail');
+            var itemsContainer = element.querySelector('.items-container');
 
-    return function($scope, element, $attr) {
-        element.on('click', function() {
-            var inc = JSON.parse($attr.testingStuff);
-            console.log($scope.categories[inc[0]].lists[inc[1]].items);
-        });
+            angular.element(itemHeader).on('click', function() {
+                $scope.$apply(function() {
+                    $scope.item.selected = !$scope.item.selected;
+                    $state.go('app.category.list.item', {
+                        categoryId: $scope.selectedCategoryId,
+                        listId: $scope.list.id,
+                        itemId: $scope.item.id
+                    });
+                });
+            });
+        }
     };
 
 });
@@ -217,5 +266,20 @@ forgetlessApp.controller('LoginController', function($scope) {
     };
 
     $scope.testfunc();
+
+});
+
+forgetlessApp.run(function($rootScope, $urlRouter, $stateParams, $window) {
+    // TODO this causes delay that onpopstate fixes, how the fuck do i shoot this web?!
+    $rootScope.$on('$locationChangeSuccess', function(evt) {
+        // Halt state change from even starting
+        evt.preventDefault();
+        $urlRouter.sync();
+        console.log($stateParams);
+    });
+
+    angular.element($window).bind('popstate', function() {
+        console.log('pop', $stateParams);
+    });
 
 });
