e2ea0b85c3816a0a9ee66fc5792f4debd1f190df
Greg Joy <greg@gregjoy.co.uk>
2014-02-14 14:45:25 +0000

    ensured stack dump uses async.each()

--- forgetless-app/forgetless/helpers/stack_helper.js
+++ forgetless-app/forgetless/helpers/stack_helper.js
@@ -2,7 +2,7 @@ module.exports = {
     getCompleteJSONStackDump:function(userId, callback){
         GLOBAL.async.waterfall(
             [
-                // Gets all category links for a specified user
+                // Gets dump safe user (doesnt contain hashes etc)
                 function(callback){
                     GLOBAL.defs.User(null, null, function(err, user) {
                         user.loadDumpSafeUserFromId(userId, function(err, safeUser) {
@@ -21,25 +21,44 @@ module.exports = {
                 },
                 // Gets the category linked to the category links
                 function(user, callback){
-                    var count = 0;
+
                     // validates that there are category lists, skips if not
                     if(user.CategoryLinks == undefined) {
                         callback(null, user);
                     } else {
+
                         if(user.CategoryLinks.length > 0) {
-                            for(var inc = 0; inc < user.CategoryLinks.length; inc++) {
-                                var index = inc;
-                                GLOBAL.defs.Category(user.CategoryLinks[inc].categoryId, null, function(err, category) {
-                                    GLOBAL.defs.Audit(category.auditId, null, function(err, audit) {
-                                        category.Audit = audit;
-                                        user.CategoryLinks[index].Category = category;
-                                        count++;
-                                        if(count == user.CategoryLinks.length) {
-                                            callback(null, user);
-                                        }
+                            // iterates through all the category links
+                            GLOBAL.async.each(
+                                // array
+                                user.CategoryLinks,
+                                // iterator function
+                                function(item, callback) {
+
+                                    var index = user.CategoryLinks.indexOf(item);
+                                    var categoryId = user.CategoryLinks[index].categoryId;
+
+                                    // loads and instantiates the category with the category id
+                                    GLOBAL.defs.Category(categoryId, null, function(err, category) {
+                                        GLOBAL.defs.Audit(category.auditId, null, function(err, audit) {
+                                            category.Audit = audit;
+                                            // adds it to the category link
+                                            user.CategoryLinks[index].Category = category;
+                                            callback(err);
+                                        });
                                     });
-                                });
-                            }
+
+                                },
+                                // callback
+                                function(err) {
+                                    if(err) {
+                                        callback(err, null);
+                                    } else {
+                                        callback(null, user);
+                                    }
+                                }
+                            );
+
                         } else {
                             callback(true, user);
                         }
@@ -47,102 +66,211 @@ module.exports = {
                 },
                 // Gets all the list links for a category
                 function(user, callback){
-                    var count = 0;
-                    for(var inc = 0; inc < user.CategoryLinks.length; inc++) {
-                        var index = inc;
-                        GLOBAL.defs.ListLink(null, null, function(err, listLink) {
-                            listLink.GetAllListLinksForUser(userId, user.CategoryLinks[index].Category.id, function(err, listLinks){
-                                user.CategoryLinks[index].Category.ListLinks = listLinks;
-                                if(listLinks.length == 0) {
-                                    callback(true, user);
-                                } else {
-                                    count++;
-                                    if(count == user.CategoryLinks.length){
-                                        callback(null, user);
-                                    }
-                                }
-                            })
-                        });
-                    }
+
+                    GLOBAL.async.each(
+                        // array
+                        user.CategoryLinks,
+                        // iterator function
+                        function(item, callback) {
+                            var index = user.CategoryLinks.indexOf(item);
+                            var categoryId = user.CategoryLinks[index].categoryId;
+
+                            GLOBAL.defs.ListLink(null, null, function(err, listLink) {
+                                listLink.GetAllListLinksForUser(userId, categoryId, function(err, listLinks){
+                                    user.CategoryLinks[index].Category.ListLinks = listLinks;
+
+                                    callback(err, user);
+
+                                });
+                            });
+                        },
+                        // callback
+                        function(err) {
+                            if(err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, user);
+                            }
+                        }
+                    );
+
                 },
                 // Gets the list for each list link
                 function(user, callback){
                     var count = 0;
-                    for(var catInc = 0; catInc < user.CategoryLinks.length; catInc++){
-                        var categoryIndex = catInc;
-                        for(var listInc = 0; listInc < user.CategoryLinks[catInc].Category.ListLinks.length; listInc++){
-                            var listIndex = listInc;
-                            var listId = user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].listId;
-                            GLOBAL.defs.List(listId, null, function(err, list) {
-                                GLOBAL.defs.Audit(list.auditId, null, function(err, audit){
-                                    list.Audit = audit;
-                                    user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List = list;
-                                    count++;
-                                    if(count == user.CategoryLinks[categoryIndex].Category.ListLinks.length){
+
+                    GLOBAL.async.each(
+                        // category links array
+                        user.CategoryLinks,
+                        // iterator function
+                        function(categoryLink, callback) {
+
+                            var categoryIndex = user.CategoryLinks.indexOf(categoryLink);
+                            var category = categoryLink.Category;
+
+                            GLOBAL.async.each(
+                                // list link array
+                                category.ListLinks,
+                                // iterator function
+                                function(listLink, callback) {
+
+                                    var listLinkIndex = category.ListLinks.indexOf(listLink);
+                                    var listId = listLink.listId;
+
+                                    GLOBAL.defs.List(listId, null, function(err, list) {
+                                        GLOBAL.defs.Audit(list.auditId, null, function(err, audit){
+                                            list.Audit = audit;
+                                            user.CategoryLinks[categoryIndex].Category.ListLinks[listLinkIndex].List = list;
+
+                                            callback(err, user);
+
+                                        });
+                                    });
+
+                                },
+                                // callback
+                                function(err) {
+                                    if(err) {
+                                        callback(err, null);
+                                    } else {
                                         callback(null, user);
                                     }
-                                });
-                            });
+                                }
+                            )
+
+                        },
+                        // callback
+                        function(err) {
+                            if(err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, user);
+                            }
                         }
-                    }
+                    );
+
                 },
                 // Gets all the item links for each link
                 function(user, callback){
-                    var count = 0;
-                    for(var catInc = 0; catInc < user.CategoryLinks.length; catInc++){
-                        var categoryIndex = catInc;
 
-                        for(var listInc = 0; listInc < user.CategoryLinks[catInc].Category.ListLinks.length; listInc++){
-                            var listIndex = listInc;
-                            var listId = user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List.id;
-                            GLOBAL.defs.ItemLink(null, null, function(err, list){
-                                list.GetAllItemLinksForUser(userId, listId, function(err, itemLinks){
-                                    user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List.ItemLinks = itemLinks;
-                                    // validates that there are itemLinks and skips if there are none
-                                    if(itemLinks.length == 0) {
-                                        callback(true, user);
+                    GLOBAL.async.each(
+                        // category links array
+                        user.CategoryLinks,
+                        // iterator function
+                        function(categoryLink, callback) {
+
+                            var categoryIndex = user.CategoryLinks.indexOf(categoryLink);
+                            var category = categoryLink.Category;
+
+                            GLOBAL.async.each(
+                                // list link array
+                                category.ListLinks,
+                                // iterator function
+                                function(listLink, callback) {
+                                    var listLinkIndex = category.ListLinks.indexOf(listLink);
+
+                                    var listId = listLink.List.id;
+                                    GLOBAL.defs.ItemLink(null, null, function(err, list){
+                                        list.GetAllItemLinksForUser(userId, listId, function(err, itemLinks){
+                                            user.CategoryLinks[categoryIndex].Category.ListLinks[listLinkIndex].List.ItemLinks = itemLinks;
+                                            callback(err, user);
+                                        });
+                                    });
+
+                                },
+                                // callback
+                                function(err) {
+                                    if(err) {
+                                        callback(err, null);
                                     } else {
-                                        count++;
-                                        if(count == user.CategoryLinks[categoryIndex].Category.ListLinks.length){
-                                            callback(null, user);
-                                        }
+                                        callback(null, user);
                                     }
-                                });
-                            });
+                                }
+                            );
+                        },
+                        // callback
+                        function(err) {
+                            if(err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, user);
+                            }
                         }
-                    }
+                    );
+
                 },
                 // Gets the item for each item link
                 function(user, callback){
-                    var count = 0;
-                    for(var catInc = 0; catInc < user.CategoryLinks.length; catInc++){
-                        var categoryIndex = catInc;
-                        for(var listInc = 0; listInc < user.CategoryLinks[catInc].Category.ListLinks.length; listInc++){
-                            var listIndex = listInc;
-                            // validates if there are any ItemLinks and skips if not
-                            if(user.CategoryLinks[catInc].Category.ListLinks[listInc].List.ItemLinks == undefined) {
-                                callback(null, user);
-                            } else {
-                                for(var itemInc = 0; itemInc < user.CategoryLinks[catInc].Category.ListLinks[listInc].List.ItemLinks.length; itemInc++) {
-                                    var itemIndex = itemInc;
-                                    var itemId = user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List.ItemLinks[itemIndex].itemId;
-                                    GLOBAL.defs.Item(itemId, null, function(err, item){
-                                        GLOBAL.defs.ReminderHelper.GetAllRemindersForUserAndItem(false, user.id, item.id, function(err, reminders) {
-                                            item.Reminders = reminders;
-                                            GLOBAL.defs.Audit(item.auditId, null, function(err, audit){
-                                                item.Audit = audit;
-                                                user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List.ItemLinks[itemIndex].Item = item;
-                                                count++;
-                                                if(count == user.CategoryLinks[categoryIndex].Category.ListLinks[listIndex].List.ItemLinks.length){
-                                                    callback(null, user);
-                                                }
+
+                    GLOBAL.async.each(
+                        // category links array
+                        user.CategoryLinks,
+                        // iterator function
+                        function(categoryLink, callback) {
+
+                            var categoryIndex = user.CategoryLinks.indexOf(categoryLink);
+                            var category = categoryLink.Category;
+
+                            GLOBAL.async.each(
+                                // list link array
+                                category.ListLinks,
+                                // iterator function
+                                function(listLink, callback) {
+
+                                    var listLinkIndex = category.ListLinks.indexOf(listLink);
+
+                                    GLOBAL.async.each(
+                                        // list link array
+                                        listLink.List.ItemLinks,
+                                        // iterator function
+                                        function(itemLink, callback) {
+
+                                            var itemLinkIndex = listLink.List.ItemLinks.indexOf(itemLink);
+
+                                            var itemId = itemLink.itemId;
+                                            GLOBAL.defs.Item(itemId, null, function(err, item){
+                                                GLOBAL.defs.ReminderHelper.GetAllRemindersForUserAndItem(false, user.id, item.id, function(err, reminders) {
+                                                    item.Reminders = reminders;
+                                                    GLOBAL.defs.Audit(item.auditId, null, function(err, audit){
+                                                        item.Audit = audit;
+                                                        user.CategoryLinks[categoryIndex].Category.ListLinks[listLinkIndex].List.ItemLinks[itemLinkIndex].Item = item;
+                                                        callback(err, user);
+                                                    });
+                                                });
                                             });
-                                        });
-                                    });
+
+                                        },
+                                        // callback
+                                        function(err) {
+                                            if(err) {
+                                                callback(err, null);
+                                            } else {
+                                                callback(null, user);
+                                            }
+                                        }
+                                    );
+
+                                },
+                                // callback
+                                function(err) {
+                                    if(err) {
+                                        callback(err, null);
+                                    } else {
+                                        callback(null, user);
+                                    }
                                 }
+                            );
+                        },
+                        // callback
+                        function(err) {
+                            if(err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, user);
                             }
                         }
-                    }
+                    );
+
                 }
             ],
             function(err, object){
@@ -151,3 +279,5 @@ module.exports = {
         );
     }
 };
+
+
